f43b288b8b26d63da891a288466162994982451e
rajat <rajat.namdev@pokersportsleague.com>
2019-09-04 16:21:39 +0530

    changes

---
+++ src/app/qualifier/qualifier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qualifier',
+  templateUrl: './qualifier.component.html',
+  styleUrls: ['./qualifier.component.css']
+})
+export class QualifierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
