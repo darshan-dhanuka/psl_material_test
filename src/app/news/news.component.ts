6f97ca29db3392f6953c070702800e7b7eae7d71
rajat <rajat.namdev@pokersportsleague.com>
2019-09-09 14:53:26 +0530

    linking done

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PlatformLocation } from '@angular/common';
+import { AuthenticationService, UserDetails } from '../authentication.service';
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+  public show_dialog : boolean = false;
+  public show_reg : boolean = false;
+  public button_name : any = 'Show Login Form!';
+  user: SocialUser;
+  details: UserDetails;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public auth: AuthenticationService, private authService: AuthService, location: PlatformLocation) {
+    location.onPopState(() => {
+       this.load();
+   });
   }
 
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      //console.log(user);
+    });
+  }
+  load()
+{
+  window.location.reload();
+}
+
+toggle() {
+  document.getElementById('divshow2').style.display = 'block';
+}
+
+togglereg() {
+  document.getElementById('divreg').style.display = 'block';
+}
+
+signOut(): void {
+  this.authService.signOut();
+}
 }
--- src/app/player-profile/player-profile.component.ts
+++ src/app/player-profile/player-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PlatformLocation } from '@angular/common';
+import { AuthenticationService, UserDetails } from '../authentication.service';
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
 @Component({
   selector: 'app-player-profile',
   templateUrl: './player-profile.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerProfileComponent implements OnInit {
 
-  constructor() { }
+  public show_dialog : boolean = false;
+  public show_reg : boolean = false;
+  public button_name : any = 'Show Login Form!';
+  user: SocialUser;
+  details: UserDetails;
 
-  ngOnInit() {
+  constructor(public auth: AuthenticationService, private authService: AuthService, location: PlatformLocation) {
+    location.onPopState(() => {
+       this.load();
+   });
   }
 
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      //console.log(user);
+    });
+  }
+  load()
+{
+  window.location.reload();
 }
+
+toggle() {
+  document.getElementById('divshow2').style.display = 'block';
+}
+
+togglereg() {
+  document.getElementById('divreg').style.display = 'block';
+}
+
+signOut(): void {
+  this.authService.signOut();
+}
+
+}
+
