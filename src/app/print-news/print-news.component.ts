bdfbf190b8d957a08cf5d75cec3b9c067fe401de
Darshan <darshan.dhanukaa@gmail.com>
2019-09-13 18:33:15 +0530

    team and stats changes

--- src/app/print-news/print-news.component.ts
+++ src/app/print-news/print-news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
+import { AuthenticationService, UserDetails } from '../authentication.service';
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-print-news',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-news.component.css']
 })
 export class PrintNewsComponent implements OnInit {
+	public show_dialog : boolean = false;
+  public show_reg : boolean = false;
+  public button_name : any = 'Show Login Form!';
+  user: SocialUser;
+  details: UserDetails;
 
-  constructor() { }
+  constructor(public auth: AuthenticationService, private authService: AuthService, location: PlatformLocation) {
+    location.onPopState(() => {
+       this.load();
+   });
+  }
 
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      //console.log(user);
+    });
   }
+  load()
+{
+  window.location.reload();
+}
+
+toggle() {
+  document.getElementById('divshow2').style.display = 'block';
+}
+
+togglereg() {
+  document.getElementById('divreg').style.display = 'block';
+}
+
+signOut(): void {
+  this.authService.signOut();
+}
 
 }
